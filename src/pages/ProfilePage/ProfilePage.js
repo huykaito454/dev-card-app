86d9994e4b2f2a0e085dd63fe047ed6aeee82f8b
huykaito454 <tronghuy.832001@gmail.com>
2022-06-30 13:22:46 +0700

    commit7

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -4,8 +4,8 @@ const ProfilePage = () => {
   return (
     <div className="page-container mt-10 p-2 flex flex-col items-center">
       <div className="flex items-start justify-between">
-        <div className="w-[60%] flex items-start justify-between border-r-2 pr-4 border-white">
-          <div className="text-justify w-[96%] mb-10">
+        <div className="w-[65%] flex items-start justify-between pr-4 ">
+          <div className="text-justify w-[95%]">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum
             dolores possimus atque, iste accusamus assumenda odit placeat
             veniam, culpa consectetur est eum nobis veritatis modi eveniet totam
@@ -19,9 +19,45 @@ const ProfilePage = () => {
           </div>
           <i className="fas fa-pencil-alt text-2xl"></i>
         </div>
-        <div className="flex items-start justify-between"></div>
+        <div className="flex items-start justify-between pl-4 w-[35%] border-l-2 ">
+          <div className="flex flex-col items-end gap-y-4 w-[35%] ">
+            <p className="text-4xl font-semibold">nthuy</p>
+            <div>
+              <p>8/3/2001</p>
+              <p className=" font-semibold">Developer</p>
+            </div>
+            <div className="flex flex-col items-end gap-y-2">
+              <span className=" text-xs">Linked account</span>
+              <div className="socials flex items-center">
+                <img
+                  src="https://img.icons8.com/fluency/35/000000/facebook-new.png"
+                  alt=""
+                />
+                <img
+                  src="https://img.icons8.com/fluency/35/000000/instagram-new.png"
+                  alt=""
+                />
+                <img
+                  src="https://img.icons8.com/fluency/35/000000/github.png"
+                  alt=""
+                />
+                <img
+                  src="https://img.icons8.com/fluency/35/000000/linkedin.png"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div className="w-[60%] max-h-[290px] overflow-hidden rounded-xl">
+            <img
+              src="https://images.unsplash.com/photo-1609505848912-b7c3b8b4beda?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80"
+              alt=""
+              className="w-full object-cover"
+            />
+          </div>
+        </div>
       </div>
-      <div className=""></div>
+      <div className="mt-10"></div>
     </div>
   );
 };
