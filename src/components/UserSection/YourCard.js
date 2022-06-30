a40ed2682e63f207c99d4c833e51f64112eb39ea
Thanh Loi <thanhloi030301@gmail.com>
2022-06-30 13:44:01 +0700

    card update active button

--- src/components/UserSection/YourCard.js
+++ src/components/UserSection/YourCard.js
@@ -6,14 +6,14 @@ const YourCard = () => {
         <div>
             <div className="head-card flex justify-between mb-12">
                 <div className="your-option flex border-2 border-pink-500 cursor-pointer">
-                    <div className="your-card flex items-center justify-center bg-primary w-36 h-8 text-lg font-bold">
+                    <div className="your-card button-active flex items-center justify-center bg-primary w-36 h-8 text-lg font-bold">
                         Card
                     </div>
                     <div className="your-template button-active flex items-center justify-center  w-32 h-8 text-lg font-bold cursor-pointer">
                         Template
                     </div>
                 </div>
-                <div className="add flex items-center justify-center bg-primary w-36 h-8 text-lg font-bold cursor-pointer">
+                <div className="add button-active flex items-center justify-center bg-primary w-36 h-8 text-lg font-bold cursor-pointer">
                     Add
                 </div>
             </div>
