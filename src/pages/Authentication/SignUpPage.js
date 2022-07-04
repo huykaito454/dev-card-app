8150dbe277677c56c67556823a8f85b6bc09e58c
nhatngynxd <19110256@student.hcmute.edu.vn>
2022-07-04 13:41:48 +0700

    active button

--- src/pages/Authentication/SignUpPage.js
+++ src/pages/Authentication/SignUpPage.js
@@ -1,45 +1,42 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import Input from "./Input";
-import * as yup from "yup";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Input from './Input';
+import * as yup from 'yup';
 const SignUpPage = () => {
   const { control } = useForm();
   return (
-    <div className="bg-register h-screen bg-cover bg-no-repeat flex items-center justify-center">
-      <div className="form-sign-up flex flex-col items-center justify-center bg-[#2B2B2B] rounded-xl md:px-48 px-20 py-5">
-        <div className="title text-3xl font-bold mt-8 mb-5">
+    <div className='bg-register h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='form-sign-up flex flex-col items-center justify-center bg-[#2B2B2B] rounded-xl md:px-48 px-20 py-5'>
+        <div className='title text-3xl font-bold mt-8 mb-5'>
           Creating
-          <span className="text-primary"> Account</span>
+          <span className='text-primary'> Account</span>
         </div>
-        <div className="social text-primary mb-6 text-3xl flex justify-between w-[50%]">
-          <i className="fab fa-google cursor-pointer"></i>
-          <i className="fab fa-facebook cursor-pointer"></i>
-          <i className="fab fa-github cursor-pointer"></i>
+        <div className='social text-primary mb-6 text-3xl flex justify-between w-[50%]'>
+          <i className='fab fa-google cursor-pointer'></i>
+          <i className='fab fa-facebook cursor-pointer'></i>
+          <i className='fab fa-github cursor-pointer'></i>
         </div>
-        <div className="tips text-[#ADADAD] text-xs font-bold mb-4">
+        <div className='tips text-[#ADADAD] text-xs font-bold mb-4'>
           Or using your email for registration
         </div>
-        <div className="form flex flex-col w-[100%] gap-5 text-black text-xs ">
+        <div className='form flex flex-col w-[100%] gap-5 text-black text-xs '>
           <Input
-            placeholder="Email"
-            icon={"fas fa-envelope"}
-            name="email"
-            control={control}
-          ></Input>
+            placeholder='Email'
+            icon={'fas fa-envelope'}
+            name='email'
+            control={control}></Input>
           <Input
-            placeholder="Password"
-            icon={"fas fa-key"}
-            name="password"
-            control={control}
-          ></Input>
+            placeholder='Password'
+            icon={'fas fa-key'}
+            name='password'
+            control={control}></Input>
           <Input
-            placeholder="Confirm Password"
-            icon={"fas fa-key"}
-            name="confirmPassword"
-            control={control}
-          ></Input>
+            placeholder='Confirm Password'
+            icon={'fas fa-key'}
+            name='confirmPassword'
+            control={control}></Input>
         </div>
-        <div className="btn-sign-up bg-primary text-sm font-normal rounded-3xl px-6 py-2 flex justify-center items-center mt-8 mb-8 cursor-pointer">
+        <div className='btn-sign-up bg-primary text-sm font-normal rounded-3xl px-6 py-2 flex justify-center items-center mt-8 mb-8 cursor-pointer button-active'>
           SIGN UP
         </div>
       </div>
